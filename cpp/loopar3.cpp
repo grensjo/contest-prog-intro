4095f028c5df9358a45072d02f189c14f15ca38f
Johan Sannemo <johan.sannemo@gmail.com>
2012-08-04 14:07:32 +0200

    Fixed compilation error and began section on functions.

--- cpp/loopar3.cpp
+++ cpp/loopar3.cpp
@@ -3,23 +3,24 @@
 using namespace std;
 
 int main(){
-    //Slinga utan uppdatering:
+    //Loop utan uppdatering:
     for(int i = 1; i <= 16; ){
         i = i*2;
         cout << i << endl;
     }
     int moo = 64;
-    //Slinga utan initialisering:
+    //Loop utan initialisering:
     for(; moo <= 512; moo *= 2){
         cout << moo << endl;
     }
-    //Oändlig slinga:
+    //Oändlig loop:
     for(;;){
         cout << moo << endl;
         moo *= 2;
 
         //Vi kan dock använda nyckelordet 'break'
-        //för att bryta oss ur en slinga:
+        //för att bryta oss ur en loop
+:
         if(moo > 4096){
             break;
         }
